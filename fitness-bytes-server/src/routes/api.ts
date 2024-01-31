8eff560420c7754baad5ac175c1ce66c8ca2cf4d
Jason Paciorek <paciorekjason@gmail.com>
2024-01-31 14:44:59 -0600

    Added middleware to verify json and completed api router

--- fitness-bytes-server/src/routes/api.ts
+++ fitness-bytes-server/src/routes/api.ts
@@ -2,74 +2,95 @@
 import { Router } from "express";
 import { toggleLike, addPost, deletePost, editPost, validateIsOwner, isLiked } from "../controllers/postsController";
 import { ObjectId } from "mongodb";
-import Payload from "../interfaces/Payload";
 import authMiddleware from "../middleware/authMiddleware";
+import jsonMiddleware from "../middleware/jsonMiddleware";
+import Post from "../models/Post";
 
 export interface APIResponse {
     message: string;
     result?: boolean | number; 
 }
 
-const router = Router()
+const routerAPI = Router()
+
+routerAPI.use(jsonMiddleware<APIResponse>); // Ensures all route have json/application headers properly set returns a APIResponse if fails
 
 // *** API Routes ***
 //authMiddleware
-router.post("/api/likePost", async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-        return res.status(400).send("Error: Expect Content-Type application/json");
-    }
+routerAPI.post("/api/likePost", async (req, res) => {
 
     const body = req.body || {};
     const userID = body.userID || "";
     const postID = body.postID || "";
 
     if (postID === "" || userID === "") {
-        return res.status(400).send("No postID or userID");
-    }
 
-    try {
-        const result = await toggleLike(postID, userID);
-        res.status(200).json(result); // Send JSON response
-    } catch (error) {
-        console.error("Error in /api/likePost:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-// authMiddleware
-router.post("/api/addPost", async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-        return res.status(400).send("Error: Expect Content-Type application/json");
-    }
-
-    const body = req.body || {};
-    const userID = parseInt(body.userID) || "";
-    const content = body.content || "";
-    const username = body.username || "";
-
-    if (userID === "" || content === "" || username === "") {
-        return res.status(400).send("No UserID or Content or Username");
-    }
-
-    try {
-        const result = await addPost(userID, username, content);
-        res.status(201).json(result); // Send JSON response
-    } catch (error) {
-        console.error("Error in /api/addPost:", error);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-//authMiddleware
-router.delete("/api/deletePost", async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-        
         const payload: APIResponse = {
-            message: "Error: Expect Content-Type application/json",
+            message: "No postID or userID",
         }
 
         return res.status(400).json(payload);
     }
+
+    try {
+
+        const payload: APIResponse = {
+            message: "",
+            result: await toggleLike(postID, userID)
+        }
+
+        res.status(200).json(payload);
+    } catch (error) {
+
+        const payload: APIResponse = {
+            message: "Internal Server Error",
+        }
+
+        console.error("Error in /api/likePost:", error);
+        res.status(500).json(payload);
+    }
+});
+
+// authMiddleware
+routerAPI.post("/api/addPost", async (req, res) => {
+
+    const body = req.body || {};
+    const userID = body.userID;
+    const content = body.content || "";
+    const username = body.username || "";
+
+    if (userID === "" || content === "" || username === "") {
+
+        const payload: APIResponse = {
+            message: "No UserID or Content or Username",
+        }
+
+        return res.status(400).json(payload);
+    }
+
+    try {
+
+        const newPost: Post = {
+            userId: userID,
+            username: username,
+            content: content,
+        }
+
+        const result = await addPost(newPost);
+        res.status(201).json(result);
+    } catch (error) {
+
+        const payload: APIResponse = {
+            message: "Internal Server Error",
+        }
+
+        console.error("Error in /api/addPost:", error);
+        res.status(500).json(payload);
+    }
+});
+
+//authMiddleware
+routerAPI.delete("/api/deletePost", async (req, res) => {
 
     const body = req.body || {};
     
@@ -104,15 +125,7 @@ router.delete("/api/deletePost", async (req, res) => {
 });
 
 //authMiddleware
-router.patch("/api/editPost", async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-
-        const payload: APIResponse = {
-            message: "Error: Expect Content-Type application/json",
-        }
-
-        return res.status(400).json(payload);
-    }
+routerAPI.patch("/api/editPost", async (req, res) => {
 
     const body = req.body || {};
 
@@ -148,15 +161,7 @@ router.patch("/api/editPost", async (req, res) => {
 });
 
 // *** Verifys that someone is the owner of a post ***
-router.post(`/api/postOwner`, async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-
-        const payload: APIResponse = {
-            message: "Error: Expect Content-Type application/json",
-        }
-
-        return res.status(400).json(payload);
-    }
+routerAPI.post(`/api/postOwner`, async (req, res) => {
 
     const body = req.body || {};
 
@@ -191,15 +196,7 @@ router.post(`/api/postOwner`, async (req, res) => {
     }
 });
 
-router.post('/api/isLiked', async (req, res) => {
-    if (req.get("Content-Type") !== "application/json") {
-        
-        const payload: APIResponse = {
-            message: "Error: Expect Content-Type application/json",
-        }
-
-        return res.status(400).json(payload);
-    }
+routerAPI.post('/api/isLiked', async (req, res) => {
 
     const body = req.body || {};
 
@@ -234,3 +231,5 @@ router.post('/api/isLiked', async (req, res) => {
         res.status(500).json(payload);
     }
 });
+
+export default routerAPI;
