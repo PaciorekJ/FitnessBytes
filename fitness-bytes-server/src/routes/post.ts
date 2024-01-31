19ffe258328944ce1c756e38956ef8d2865b8c88
Jason Paciorek <paciorekjason@gmail.com>
2024-01-31 15:00:22 -0600

    Added routers for post and user routes

---
+++ fitness-bytes-server/src/routes/post.ts
@@ -0,0 +1,108 @@
+
+import { Router, Response, Request } from "express";
+import PageQuery from "../interfaces/PageQuery";
+import { getLikedPosts, getMostLikedPosts, getNewestPosts, getPostCountByUserId, getPostLikesByUserId } from "../controllers/postsController";
+import { getUserIDFromUsername } from "../controllers/userController";
+import Payload from "../interfaces/Payload";
+
+const routerPost = Router();
+
+routerPost.get("/feed/:username", async (req: Request & {query: PageQuery}, res: Response) => {
+    try {
+        const username = req.params.username || "No-Username";
+        const userID = await getUserIDFromUsername(username)
+
+        if (!userID) {
+            const payload: Payload = {
+                message: "Invalid Username",
+            }
+
+            return res.status(400).json(payload);
+        }
+
+        const sortBy = req.query.sortBy || 'newest';
+        
+        let posts;
+
+        // Get posts according to selectedSortBy
+        if (sortBy === "liked") {
+            posts = await getLikedPosts(userID, req.query);
+        } else if (sortBy === "most-liked") {
+            posts = await getMostLikedPosts(req.query);
+        } else if (sortBy === "newest"){
+            posts = await getNewestPosts(req.query);
+        }
+
+        const payload: Payload = {
+            message: "",
+            posts: posts || [],
+            username: username || "",
+            userID: userID,
+            pagenumber: parseInt(req.query.pageNumber || "0")
+        }
+
+        res.json(payload);
+
+    } catch (error) {
+        const payload: Payload = {
+            message: "Internal Server Error"
+        }
+        
+        console.error("Error in /feed route:", error);
+        res.status(500).send(payload);
+    }
+});
+
+routerPost.get("/accountPage/:username", async (req: Request & {query: PageQuery}, res: Response) => {
+    try {
+        const username = req.params.username || "No-Username";
+        const userID = await getUserIDFromUsername(username)
+
+        if (!userID) {
+            const payload: Payload = {
+                message: "Invalid Username",
+            }
+
+            return res.status(400).json(payload);
+        }
+
+        const pageNumber = parseInt(req.query.pageNumber || "0");
+        const sortBy = req.query.sortBy || 'newest';
+
+        const postCount = await getPostCountByUserId(userID);
+        const likeCount = await getPostLikesByUserId(userID);
+
+        let posts;
+
+        // Get posts according to selectedSortBy
+        if (sortBy === "liked") {
+            posts = await getLikedPosts(userID, req.query, userID);
+        } else if (sortBy === "most-liked") {
+            posts = await getMostLikedPosts(req.query, userID);
+        } else if (sortBy === "newest"){
+            posts = await getNewestPosts(req.query, userID);
+        }
+
+        const payload: Payload = {
+            message: "",
+            posts: posts || [],
+            username: username,
+            userID: userID,
+            pagenumber: pageNumber,
+            postNumber: postCount,
+            likesNumber: likeCount
+        }
+
+        res.json(payload);
+    } catch (error) {
+
+        const payload: Payload = {
+            message: "Internal Server Error",
+        }
+
+        console.error("Error in /accountPage route:", error);
+        res.status(500).send(payload);
+    }
+});
+
+export default routerPost;
