6be7ba1c3602bd81225510fbc9056675041d2eaa
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-01 15:42:54 -0600

    Added a reusable API Client for handling endpoints

---
+++ fitness-bytes-frontend/src/interfaces/LoginResponse.ts
@@ -0,0 +1,7 @@
+
+interface LoginResponse {
+	message: string;
+	token: string;
+}
+
+export default LoginResponse
--- fitness-bytes-frontend/src/main.tsx
+++ fitness-bytes-frontend/src/main.tsx
@@ -6,14 +6,6 @@ import Login from "./pages/Login/Login";
 
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-// --color-primary: rgba(158, 138, 95, .5);
-// 	--color-primary-2: rgba(192, 185, 160, 1);
-// 	--color-accent: #647c42;
-// 	--color-neutral: #2e250b;
-
-//     --color-accent-warm: rgba(171, 176, 142, 1);
-//     --color-accent-warm-lighter: rgba(192, 185, 160, 1);
-
 const theme = createTheme({
 	palette: {
 		primary: {
