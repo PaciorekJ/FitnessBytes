c823418d2f3013411652b18d446a84a62567535f
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-01 16:20:08 -0600

    Added router to project

--- fitness-bytes-server/src/index.ts
+++ fitness-bytes-server/src/index.ts
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 const app: Express = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/api', routerAPI);
