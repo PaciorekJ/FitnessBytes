2dbe0325df0b3ab36a7908ec980f377ebaa051fd
Jason Paciorek <paciorekjason@gmail.com>
2024-02-09 17:17:10 -0600

    Fixed up error pages layout

--- fitness-bytes-frontend/src/pages/Error.tsx
+++ fitness-bytes-frontend/src/pages/Error.tsx
@@ -6,87 +6,102 @@ import {
 	List,
 	ListItem,
 	Stack,
-	Typography,
+	Typography
 } from "@mui/material";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
-import LogoName from "../components/LogoName";
+import LogoIcon from "../components/LogoIcon";
 
 const Error = () => {
 	const error = useRouteError();
 
 	if (isRouteErrorResponse(error)) {
 		return (
-			<Stack paddingTop={4} maxWidth={"100%"} gap={4}>
-				<LogoName center />
-				<Stack gap={4}>
-					<Typography textAlign={"center"} variant="h2">
-						{error.status}: {error.statusText}
-					</Typography>
-					{error.data?.message && (
-						<Typography variant="body1">{error.data.message}</Typography>
-					)}
-					<Divider orientation="horizontal" variant="middle" flexItem>
-						<ErrorIcon />
-					</Divider>
-					<Grid container spacing={4} columns={{ xs: 1, md: 2 }}>
-						<Grid item xs={1} md={1}>
-							<Typography
-								sx={{ fontSize: "2rem" }}
-								letterSpacing={1.05}
-								lineHeight={0.95}
-								variant="h3">
-								We are oh so human, and sometimes we make mistakes.
-							</Typography>
-							<Typography
-								sx={{ fontSize: "1.5rem" }}
-								letterSpacing={1.01}
-								variant="h4"
-								lineHeight={0.95}
-								paddingTop={4}>
-								We apologize that you've ecountered this!
-							</Typography>
-						</Grid>
-						<Grid item xs={1} md={1}>
-							<List
-								sx={{
-									"fontSize": "1.2rem",
-									"& .MuiListItem-root": {
-										display: "list-item",
-									},
-								}}
-								subheader={
-									"Here are some helpful links to get you where you want to go!"
-								}>
-								<ListItem>
-									<Typography>
-										Start fresh with us at the{" "}
-										<Link sx={{ color: "var(--color-accent)" }} href="/">
-											Home
+			<Grid>
+				<LogoIcon centerScreen />
+				<Grid
+					item
+					paddingTop={-1}
+					sx={{
+						position: "absolute",
+						top: "50%",
+						left: "50%",
+						transform: "translate(-50%, -50%)",
+						paddingX: { xs: 0, xl: 10 },
+						minHeight: "min-content",
+						margin: "auto",
+					}}
+					width={{ xs: "100%", lg: "80%" }}
+					gap={4}>
+					<Stack marginTop={5} gap={4}>
+						<Typography textAlign={"center"} variant="h2">
+							{error.status}: {error.statusText}
+						</Typography>
+						{error.data?.message && (
+							<Typography variant="body1">{error.data.message}</Typography>
+						)}
+						<Divider orientation="horizontal" variant="middle" flexItem>
+							<ErrorIcon />
+						</Divider>
+						<Grid container columns={{ xs: 1, md: 2 }}>
+							<Grid item margin={"auto"} maxWidth={"400px"} xs={1}>
+								<Typography
+									letterSpacing={1.05}
+									lineHeight={0.95}
+									variant="h4"
+									component="h2">
+									Oops, sometimes we make mistakes.
+								</Typography>
+								<Typography
+									letterSpacing={1.01}
+									variant="h5"
+									component="h3"
+									lineHeight={0.95}
+									paddingY={4}>
+									We apologize that you've ecountered this!
+								</Typography>
+							</Grid>
+							<Grid item margin={"auto"} padding={0} maxWidth={"400px"} xs={1}>
+								<List
+									sx={{
+										"fontSize": "1.2rem",
+										"& .MuiListItem-root": {
+											display: "list-item",
+										},
+									}}
+									subheader={
+										"Here are some helpful links to get you where you want to go!"
+									}>
+									<ListItem>
+										<Typography>
+											Start fresh with us at the{" "}
+											<Link color="secondary" href="/">
+												Home
+											</Link>{" "}
+											page
+										</Typography>
+									</ListItem>
+									<ListItem>
+										<Typography>
+											A returning user? Go to the{" "}
+											<Link color="secondary" href="/login">
+												Login
+											</Link>{" "}
+											page
+										</Typography>
+									</ListItem>
+									<ListItem>
+										Ready to get signed up? Check out the{" "}
+										<Link color="secondary" href="/signup">
+											Signup
 										</Link>{" "}
 										page
-									</Typography>
-								</ListItem>
-								<ListItem>
-									<Typography>
-										A returning user? Go to the{" "}
-										<Link sx={{ color: "var(--color-accent)" }} href="/login">
-											Login
-										</Link>{" "}
-										page
-									</Typography>
-								</ListItem>
-								<ListItem>
-									Ready to get signed up? Check out the{" "}
-									<Link sx={{ color: "var(--color-accent)" }} href="/signup">
-										Signup
-									</Link>{" "}
-									page
-								</ListItem>
-							</List>
+									</ListItem>
+								</List>
+							</Grid>
 						</Grid>
-					</Grid>
-				</Stack>
-			</Stack>
+					</Stack>
+				</Grid>
+			</Grid>
 		);
 	} else {
 		return (
