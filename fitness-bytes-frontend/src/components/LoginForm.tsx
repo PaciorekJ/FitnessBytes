4b47a3c61ff1aef86b30fc8af25fb2a6bbc6d99c
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-09 13:32:24 -0600

    Refactored login form logic to LoginForm

---
+++ fitness-bytes-frontend/src/components/LoginForm.tsx
@@ -0,0 +1,93 @@
+import {
+	Alert,
+	Button,
+	FormControl,
+	Link,
+	Stack,
+	TextField,
+} from "@mui/material";
+import { useState } from "react";
+import { useCookies } from "react-cookie";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import PasswordInput from "./PasswordInput";
+import LoginResponse from "../interfaces/LoginResponse";
+import ClientService from "../services/ClientService";
+import { FormData } from "../services/ValidatorService";
+
+const LoginForm = () => {
+	const [failedLogin, setFailedLogin] = useState(false);
+	const [, setCookie] = useCookies(["token"]);
+
+	const {
+		register,
+		handleSubmit,
+		formState: { isDirty, isValid },
+	} = useForm<FormData>();
+
+	const navigator = useNavigate();
+
+	async function handleLogin(data: FormData) {
+		interface D {
+			response: {
+				status: number;
+			};
+		}
+
+		const client = new ClientService<LoginResponse>("user/login");
+
+		try {
+			const response = await client.post(data);
+			const { token } = response;
+
+			// Store token in cookie upon successful login
+			setCookie("token", token, { path: "/" });
+
+			navigator("/login/" + data.username);
+		} catch (e) {
+			if ((e as D).response.status === 401) {
+				setFailedLogin(true);
+				return;
+			}
+
+			console.error(e);
+		}
+	}
+
+	return (
+		<form
+			className="stack"
+			onSubmit={handleSubmit((data) => handleLogin(data))}>
+			{failedLogin && (
+				<Alert severity="error">Invalid Username or Password</Alert>
+			)}
+			<FormControl>
+				<TextField
+					id="username"
+					fullWidth
+					variant="outlined"
+					label="Username"
+					{...register("username", { required: true })}
+				/>
+			</FormControl>
+			<FormControl>
+				<PasswordInput register={register} options={{ required: true }} />
+			</FormControl>
+			<Stack>
+				<Button
+					disabled={!isValid || !isDirty}
+					variant="contained"
+					color="secondary"
+					size="large"
+					type="submit">
+					Log In
+				</Button>
+				<Link href="/signup" variant="overline" underline="hover">
+					Don't have an account yet?
+				</Link>
+			</Stack>
+		</form>
+	);
+};
+
+export default LoginForm;
--- fitness-bytes-frontend/src/pages/Login/Login.tsx
+++ fitness-bytes-frontend/src/pages/Login/Login.tsx
@@ -1,61 +1,12 @@
 import {
-	Alert,
-	Button,
-	FormControl,
-	Link,
 	Stack,
-	TextField,
-	Typography,
+	Typography
 } from "@mui/material";
-import { useState } from "react";
-import { useCookies } from "react-cookie";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
+import LoginForm from "../../components/LoginForm";
 import Logo from "../../components/Logo";
-import PasswordInput from "../../components/PasswordInput";
-import LoginResponse from "../../interfaces/LoginResponse";
-import ClientService from "../../services/ClientService";
-import { FormData } from "../../services/ValidatorService";
 import "./index.css";
 
 const Login = () => {
-	const [failedLogin, setFailedLogin] = useState(false);
-	const [, setCookie] = useCookies(["token"]);
-
-	const {
-		register,
-		handleSubmit,
-		formState: { isDirty, isValid },
-	} = useForm<FormData>();
-
-	const navigator = useNavigate();
-
-	async function handleLogin(data: FormData) {
-		interface D {
-			response: {
-				status: number;
-			};
-		}
-
-		const client = new ClientService<LoginResponse>("user/login");
-
-		try {
-			const response = await client.post(data);
-			const { token } = response;
-
-			// Store token in cookie upon successful login
-			setCookie("token", token, { path: "/" });
-
-			navigator("/login/" + data.username);
-		} catch (e) {
-			if ((e as D).response.status === 401) {
-				setFailedLogin(true);
-				return;
-			}
-
-			console.error(e);
-		}
-	}
 
 	return (
 		<Stack gap={4} padding={10} alignItems={"center"}>
@@ -69,38 +20,7 @@ const Login = () => {
 				</Typography>
 				<Logo center />
 			</Stack>
-			<form
-				className="stack"
-				onSubmit={handleSubmit((data) => handleLogin(data))}>
-				{failedLogin && (
-					<Alert severity="error">Invalid Username or Password</Alert>
-				)}
-				<FormControl>
-					<TextField
-						id="username"
-						fullWidth
-						variant="outlined"
-						label="Username"
-						{...register("username", { required: true })}
-					/>
-				</FormControl>
-				<FormControl>
-					<PasswordInput register={register} options={{ required: true }} />
-				</FormControl>
-				<Stack>
-					<Button
-						disabled={!isValid || !isDirty}
-						variant="contained"
-						color="secondary"
-						size="large"
-						type="submit">
-						Log In
-					</Button>
-					<Link href="/signup" variant="overline" underline="hover">
-						Don't have an account yet?
-					</Link>
-				</Stack>
-			</form>
+			<LoginForm />
 		</Stack>
 	);
 };
