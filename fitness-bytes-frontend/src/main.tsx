d5280c64fd73e2aafcd297aefc4defaae9729ee9
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-09 13:05:04 -0600

    Redefined color pallete and created FitnessBytes Logo

--- fitness-bytes-frontend/src/main.tsx
+++ fitness-bytes-frontend/src/main.tsx
@@ -2,18 +2,39 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { RouterProvider } from "react-router-dom";
 import router from "./Router";
 
+const colors = {
+	darkBlue: "#010110",
+	lightGreenishWhite: "#f1fbef",
+	vibrantOrange: "#f47227",
+	teal: "#11b4bd",
+	navyBlue: "#163a63",
+};
+
+// Create a theme instance
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: "#2e250b",
+			main: colors.navyBlue, // Used for primary buttons and links
+			contrastText: colors.lightGreenishWhite, // Text color for primary buttons
 		},
 		secondary: {
-			main: "#647c42",
+			main: colors.teal, // Used for secondary buttons and accents
+			contrastText: colors.darkBlue, // Text color for secondary buttons
+		},
+		error: {
+			main: colors.vibrantOrange, // Used for error states and icons
+		},
+		background: {
+			default: colors.lightGreenishWhite, // Replace with the hex color code for the page background
+			paper: colors.lightGreenishWhite, // Replace with the hex color code for the background of paper elements
+		},
+		text: {
+			primary: colors.darkBlue, // Primary text color
+			secondary: colors.navyBlue,
 		},
 	},
 });
@@ -21,7 +42,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
-			<RouterProvider router={router}/>
+			<RouterProvider router={router} />
 		</ThemeProvider>
 	</React.StrictMode>,
 );
