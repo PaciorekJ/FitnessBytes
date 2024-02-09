d151857b52d1deaa9de8b8540ec4f93d71e7391c
Jason Paciorek <paciorekjason@gmail.com>
2024-02-09 13:27:44 -0600

    Refactored Signup form logic to SignupForm Component

--- fitness-bytes-frontend/src/pages/Signup/Signup.tsx
+++ fitness-bytes-frontend/src/pages/Signup/Signup.tsx
@@ -1,103 +1,35 @@
-import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
-import CloseIcon from "@mui/icons-material/Close";
 import {
-	Alert,
-	Button,
-	FormControl,
 	Grid,
-	Link,
 	Stack,
-	TextField,
-	Typography,
+	Typography
 } from "@mui/material";
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import Logo from "../../components/Logo";
-import PasswordInput from "../../components/PasswordInput";
-import ClientService from "../../services/ClientService";
-import { FormData, schema } from "../../services/ValidatorService";
+import SignupForm from "../../components/SignupForm";
 import "./index.css";
 
 const Signup = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { isDirty, errors, isValid },
-	} = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
-
-	const navigator = useNavigate();
-
-	async function handleSignup(data: FormData) {
-		const client = new ClientService<FormData>("user/signup");
-
-		try {
-			await client.post(data);
-			navigator("/login");
-		} catch (e) {
-			console.error(e);
-		}
-	}
 
 	return (
-		<Grid
-			container
-			columns={{ xs: 1, xl: 2 }}
-			margin={"auto"}
-			gap={4}
-			paddingY={10}>
-			<Grid item xs xl>
-				<Stack>
-					<Logo />
-					<Typography fontSize={"1.5rem"} variant={"h2"}>
-						A social network for pump chasers, improvement seekers, and gym
-						lovers
-					</Typography>
-				</Stack>
-			</Grid>
-			<Grid item xs xl>
-				<form
-					className="stack"
-					onSubmit={handleSubmit((data) => handleSignup(data))}>
-					<FormControl>
-						<TextField
-							id="username"
-							fullWidth
-							variant="outlined"
-							label="Username"
-							{...register("username", { required: true })}
-						/>
-						{errors.username && (
-							<Alert icon={<CloseIcon fontSize="inherit" />} severity="error">
-								{errors.username.message}
-							</Alert>
-						)}
-					</FormControl>
-					<FormControl>
-						<PasswordInput register={register} options={{ required: true }} />
-						{errors.password && (
-							<Alert icon={<CloseIcon fontSize="inherit" />} severity="error">
-								{errors.password.message}
-							</Alert>
-						)}
-					</FormControl>
+		<Grid container paddingX={{ xs: 0, xl: 10 }} minHeight={"100vh"}>
+			<Grid
+				minHeight={"min-content"}
+				container
+				columns={{ xs: 1, xl: 2 }}
+				margin={"auto"}
+				gap={4}
+				padding={10}>
+				<Grid item xs xl>
 					<Stack>
-						<Button
-							disabled={!isValid || !isDirty}
-							variant="contained"
-							color="secondary"
-							size="large"
-							type="submit">
-							Signup
-						</Button>
-						<Link
-							display={"inline"}
-							href="/login"
-							variant="overline"
-							underline="hover">
-							Already have an account?
-						</Link>
+						<Logo />
+						<Typography fontSize={"1.5rem"} variant={"h2"}>
+							A social network for pump chasers, improvement seekers, and gym
+							lovers
+						</Typography>
 					</Stack>
-				</form>
+				</Grid>
+				<Grid item xs xl>
+					<SignupForm />
+				</Grid>
 			</Grid>
 		</Grid>
 	);
