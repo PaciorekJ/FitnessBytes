5e3cdf367858db93c1b51ae3954d6ee1adbad485
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-12 19:07:24 -0600

    Made Profile lead to user profile

--- fitness-bytes-frontend/src/pages/Feed.tsx
+++ fitness-bytes-frontend/src/pages/Feed.tsx
@@ -1,55 +1,9 @@
 import Stack from "@mui/material/Stack";
 import Post from "../interfaces/Post";
 import PostCard from "../components/PostCard";
+import posts from "../Data/filler";
 
 const Feed = () => {
-	const d = new Date();
-
-	const posts: Post[] = [
-		{
-			username: "Jason",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-		{
-			username: "Pauly D",
-			content: "Hello everyone, Today I went for a long run it was great",
-			likes: 0,
-			timeCreated: d,
-		},
-	];
-
 	return (
 		<Stack width={"100%"} alignItems={"center"}>
 			<div id="top"></div>
