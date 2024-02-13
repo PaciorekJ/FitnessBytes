b7ad00005204a1589c1a0f341c276ee6ccab8185
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-12 19:04:31 -0600

    Drafted Account page

--- fitness-bytes-frontend/src/pages/Account.tsx
+++ fitness-bytes-frontend/src/pages/Account.tsx
@@ -1,7 +1,59 @@
-import React from "react";
+import {
+	Avatar,
+	Divider,
+	Box,
+	Container,
+	Stack,
+	Typography,
+} from "@mui/material";
+import posts from "../Data/filler";
+import PostCard from "../components/PostCard";
+import { useParams } from "react-router-dom";
 
-const Account = () => {
-	return <div>Account</div>;
+const Account = ({ postNumber = 1, likesNumber = 2 }) => {
+	const { username } = useParams();
+
+	return (
+		<Container maxWidth="sm">
+			<Box
+				sx={{
+					my: 4,
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+				}}>
+				<Avatar
+					sizes="xl"
+					alt="Avatar"
+					sx={{
+						width: "20vw",
+						height: "20vw",
+						maxWidth: "200px",
+						maxHeight: "200px",
+					}}
+				/>
+				<Typography variant="h6" component="h1" gutterBottom>
+					{username}
+				</Typography>
+				<Stack flexDirection={"row"} gap={2}>
+					<Stack flexDirection={"row"} gap={"5px"}>
+						<Typography fontWeight={"700"}>{postNumber}</Typography>
+						<Typography>Posts</Typography>
+					</Stack>
+					<Stack flexDirection={"row"} gap={"5px"}>
+						<Typography fontWeight={"700"}>{likesNumber}</Typography>
+						<Typography>Likes</Typography>
+					</Stack>
+				</Stack>
+				<Divider orientation="horizontal" variant="fullWidth" />
+				<Stack>
+					{posts.map((p) => (
+						<PostCard {...p} />
+					))}
+				</Stack>
+			</Box>
+		</Container>
+	);
 };
 
 export default Account;
