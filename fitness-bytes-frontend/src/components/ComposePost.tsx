dc18d18aac29ba2828e4178d08d29c3acad66363
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-19 18:24:42 -0600

    Updated Arragne post to accomidate aria

--- fitness-bytes-frontend/src/components/ComposePost.tsx
+++ fitness-bytes-frontend/src/components/ComposePost.tsx
@@ -28,23 +28,14 @@ import { useNavigate } from "react-router-dom";
 import Post from "../interfaces/Post";
 import ClientService from "../services/ClientService";
 import { FormData, MAX_CHAR, schema } from "../services/PostValidatorService";
-
-const style = {
-	position: "absolute",
-	top: "50%",
-	left: "50%",
-	transform: "translate(-50%, -50%)",
-	width: "66%",
-	bgcolor: "background.default",
-	border: "2px solid #000",
-	borderRadius: "10px",
-	boxShadow: 24,
-	p: 4,
-};
+import ArrangePostModal from "./ArrangePostModal";
 
 const ComposePost = () => {
 	const queryClient = useQueryClient();
 	const navigator = useNavigate();
+
+	const [isOpen, setOpen] = useState(false);
+	const [error, setError] = useState("");
 
 	const username = localStorage.getItem("username") || "";
 
@@ -58,17 +49,6 @@ const ComposePost = () => {
 			},
 		);
 	};
-
-	const [isOpen, setOpen] = useState(false);
-	const [error, setError] = useState("");
-
-	const {
-		register,
-		handleSubmit,
-		reset,
-		watch,
-		formState: { isValid, isDirty, errors },
-	} = useForm<FormData>({ resolver: zodResolver(schema), mode: "all" });
 
 	const openModal = () => setOpen(true);
 	const closeModal = () => setOpen(false);
@@ -91,7 +71,6 @@ const ComposePost = () => {
 			if (_.isEmpty(res.result)) {
 				setError("Your post can not be posted at this time!");
 				setTimeout(() => {
-					reset();
 					closeModal();
 				}, 1000);
 				return;
@@ -99,16 +78,12 @@ const ComposePost = () => {
 
 			addPost(res.result as Post);
 
-			reset();
 			closeModal();
-
 			setError("");
 		} catch {
 			setError("Something went Wrong while submitting the Post");
 		}
 	};
-
-	const content = watch("content", "");
 
 	return (
 		<>
@@ -117,64 +92,17 @@ const ComposePost = () => {
 					<HistoryEduOutlinedIcon color="primary" />
 				</IconButton>
 			</Tooltip>
-			<Modal
-				open={isOpen}
-				onClose={closeModal}
-				aria-labelledby="Modal For Posting"
-				aria-describedby="Modal that is used for posting on the platform">
-				<Paper sx={style} variant="outlined">
-					<form onSubmit={handleSubmit((data) => submitPost(data))}>
-						<CardHeader
-							title={username}
-							avatar={
-								<Avatar aria-label="User Icon">{username.charAt(0)}</Avatar>
-							}
-						/>
-						<Box paddingX={2}>
-							<Divider />
-						</Box>
-						{error && <Alert color="error">{error}</Alert>}
-						{isDirty && errors.content && (
-							<Alert color="error">{errors.content.message}</Alert>
-						)}
-						<Box padding={2}>
-							<TextField
-								id="content"
-								autoFocus
-								fullWidth
-								rows={4}
-								{...register("content", { required: true })}
-								placeholder="Share A Fitness Byte..."
-								multiline
-							/>
-						</Box>
-						{isValid && (
-							<Typography>
-								{MAX_CHAR - content.length} characters left
-							</Typography>
-						)}
-						<Divider />
-						<CardActions>
-							<Stack
-								flexDirection={"row"}
-								justifyContent="space-between"
-								width="100%">
-								<Stack flexDirection={"row"}>
-									<IconButton>
-										<ImageOutlinedIcon />
-									</IconButton>
-									<IconButton>
-										<LinkOutlinedIcon />
-									</IconButton>
-								</Stack>
-								<Button variant="contained" color="secondary" type="submit">
-									Post
-								</Button>
-							</Stack>
-						</CardActions>
-					</form>
-				</Paper>
-			</Modal>
+			<ArrangePostModal
+				onSubmit={submitPost}
+				username={username}
+				buttonContent="Post"
+				ariaDescribedby="Modal that is used for posting on the platform"
+				ariaLabelledby="Modal For Posting"
+				isOpen={isOpen}
+				setOpen={setOpen}
+				error={error}
+				textValue={""}
+			/>
 		</>
 	);
 };
