e27d8661057dd7defb46a60fa191d27f6dacb7d9
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-25 22:26:25 -0600

    Abstracted Form logic for signup and login to the AuthForm component

--- fitness-bytes-frontend/src/components/SignupForm.tsx
+++ fitness-bytes-frontend/src/components/SignupForm.tsx
@@ -1,28 +1,12 @@
-import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
-import CloseIcon from "@mui/icons-material/Close";
-import {
-	Alert,
-	Button,
-	FormControl,
-	Link,
-	Stack,
-	TextField,
-} from "@mui/material";
 
-import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import ClientService from "../services/ClientService";
-import { FormData, schema } from "../services/SignupValidatorService";
-import PasswordInput from "./PasswordInput";
+import { FormData } from "../services/SignupValidatorService";
+import AuthForm from "./AuthForm";
 
 // TODO: Add Passport to handle the login
 const SignupForm = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { isDirty, errors, isValid },
-	} = useForm<FormData>({ resolver: zodResolver(schema), mode: "onChange" });
-
+	
 	const navigator = useNavigate();
 
 	async function handleSignup(data: FormData) {
@@ -37,54 +21,12 @@ const SignupForm = () => {
 	}
 
 	return (
-		<Stack
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				gap: "1.2rem",
-			}}>
-			<form onSubmit={handleSubmit((data) => handleSignup(data))}>
-				<FormControl>
-					<TextField
-						id="username"
-						fullWidth
-						variant="outlined"
-						label="Username"
-						{...register("username", { required: true })}
-					/>
-					{errors.username && (
-						<Alert icon={<CloseIcon fontSize="inherit" />} severity="error">
-							{errors.username.message}
-						</Alert>
-					)}
-				</FormControl>
-				<FormControl>
-					<PasswordInput register={register} options={{ required: true }} />
-					{errors.password && (
-						<Alert icon={<CloseIcon fontSize="inherit" />} severity="error">
-							{errors.password.message}
-						</Alert>
-					)}
-				</FormControl>
-				<Stack>
-					<Button
-						color="secondary"
-						disabled={!isValid || !isDirty}
-						variant="contained"
-						size="large"
-						type="submit">
-						Signup
-					</Button>
-					<Link
-						display="inline"
-						href="/login"
-						variant="overline"
-						underline="hover">
-						Already have an account?
-					</Link>
-				</Stack>
-			</form>
-		</Stack>
+		<AuthForm
+			formAction={handleSignup}
+			monitorFormValidity={true}
+			formButton={{ text: "Signup" }}
+			formLink={{ text: "Already have an account?", href: "/login" }}
+		/>
 	);
 };
 
