0fb018eb1cae7833833bf6462421126283090c56
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-02-26 10:48:21 -0600

    Changed FormData type to have a more semenatic name

--- fitness-bytes-frontend/src/components/AuthForm.tsx
+++ fitness-bytes-frontend/src/components/AuthForm.tsx
@@ -9,10 +9,10 @@ import {
 	TextField,
 } from "@mui/material";
 import { UseFormProps, useForm } from "react-hook-form";
-import { AuthData } from "../services/SignupValidatorService";
+import { AuthData } from "../services/AuthValidatorService";
 import PasswordInput from "./PasswordInput";
 
-import { schema } from "../services/SignupValidatorService";
+import { schema } from "../services/AuthValidatorService";
 
 interface FormAlertState {
 	state: boolean;
