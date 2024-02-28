45371cd0c7b0c8d87db06f98e3ff4d323abc30e9
Jason Paciorek <paciorekjason@gmail.com>
2024-02-27 18:29:13 -0600

    Local strategy is fully functional

--- fitness-bytes-server/src/services/passport.ts
+++ fitness-bytes-server/src/services/passport.ts
@@ -1,1 +1,45 @@
 
+import mongoose from 'mongoose';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import { validatePassword } from '../libs/Auth';
+import UserModel from '../models/user';
+
+const verifyCallback = async (username: string, password: string, done: any) => {
+    try {
+        const user = await UserModel.findOne({ username });
+        const passwordMatch = await validatePassword(username, password);
+    
+        if (passwordMatch) {
+            return done(null, user);
+        }
+
+        return done(null, false);
+    }
+    catch (e) {
+        return done(e);
+    }
+}
+
+const LocalStrategy = passportLocal.Strategy;
+const strategy = new LocalStrategy(verifyCallback);
+
+passport.use(strategy)
+passport.serializeUser((user: any, done: (error: any, userId?: string) => void) => {
+    return done(null, user._id);
+});
+passport.deserializeUser(async (id: string, done: (error: any, user?: any) => void) => {
+    try {
+        const userId = new mongoose.Types.ObjectId(id);
+        const user = await UserModel.findById(userId).lean();
+
+        if (!user) {
+            return done(null, false);
+        }
+
+        return done(null, user);
+    }
+    catch (e) {
+        return done(e);
+    }
+})
