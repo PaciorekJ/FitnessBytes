42a348b8b3f29cca819732f3c4716edce432c36f
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-03-19 20:49:40 -0500

    Sockets work, however the channels that are created require the recipientsUsername, but the recipientsId is all that is present in each notificiations

--- fitness-bytes-frontend/src/layouts/UserLayout.tsx
+++ fitness-bytes-frontend/src/layouts/UserLayout.tsx
@@ -1,22 +1,33 @@
+import { useQueryClient } from "@tanstack/react-query";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Nav from "../components/Nav";
-import UserServices from "../services/UserServices";
+import { INotification } from "../services/NotificationServices";
 import SocketServices from "../services/SocketServices";
+import UserServices from "../services/UserServices";
 
 const UserLayout = () => {
 	const navigator = useNavigate();
+	const client = useQueryClient();
 
 	useEffect(() => {
 		const authUser = async () => {
 			const username = await UserServices.isAuth();
 			if (!username) {
 				navigator("/");
-				return
+				return;
 			}
 
 			SocketServices.setUp(username);
 
+			SocketServices.registerCallback("Notification Recieved", (m) => {
+				console.log("Reached Callback -> outside");
+				client.setQueryData<INotification[]>(["notifications"], (old) => {
+					const oldNotifications = old || [];
+					console.log("Reached Callback -> inside");
+					return [...oldNotifications, m as INotification];
+				});
+			});
 		};
 
 		authUser();
