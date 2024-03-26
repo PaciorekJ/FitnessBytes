8518b983603490e945f624a2b07f7634ff80a33c
Jason Paciorek <paciorekjason@gmail.com>
2024-03-25 20:52:00 -0500

    Added Custom scroll bar

--- fitness-bytes-frontend/src/layouts/Theme.tsx
+++ fitness-bytes-frontend/src/layouts/Theme.tsx
@@ -17,13 +17,26 @@ const Theme = ({ children }: Props) => {
 	const { mode } = useThemeStore();
 
 	const theme = createTheme(getTheme(mode));
-
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<GlobalStyles
 				styles={(theme) => ({
-					body: {
+					"&::-webkit-scrollbar": {
+						"margin-block": "1em",
+						"border-radius": "100vw",
+						"backgroundColor": theme.palette.background.paper,
+						"width": "1em",
+					},
+					"&::-webkit-scrollbar-thumb": {
+						"background-image":
+							mode === "dark"
+								? `linear-gradient(to left,  ${theme.palette.primary.contrastText}, ${theme.palette.secondary.main})`
+								: `linear-gradient(to left, ${theme.palette.secondary.contrastText}, ${theme.palette.primary.main})`,
+						"border-radius": "100vw",
+						"border-color": "black",
+					},
+					"body": {
 						backgroundColor: theme.palette.background.default,
 						color: theme.palette.text.primary,
 					},
