9956eea24ea35878e6800b993ea86c87a5f129cc
Jason Paciorek <jasonpaciorek@yahoo.com>
2024-04-05 10:55:30 -0500

    Added ReplyModel

---
+++ fitness-bytes-server/src/models/Reply.ts
@@ -0,0 +1,25 @@
+
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IReply extends Document {
+  userId: mongoose.Types.ObjectId;
+  postId: mongoose.Types.ObjectId; // Reference to the root post
+  parentReplyId?: mongoose.Types.ObjectId;
+  content: string;
+  likes: number;
+  timeCreated: Date;
+}
+
+const ReplySchema: Schema = new Schema({
+  userId: { type: Schema.Types.ObjectId, required: true, index: true, ref: 'User' },
+  postId: { type: Schema.Types.ObjectId, required: true, index: true, ref: 'Post' },
+  content: { type: String, required: true },
+  parentReplyId: { type: Schema.Types.ObjectId, index: true, ref: 'Reply', default: null }, // Optional, for replies to replies
+  likes: { type: Number, default: 0 },
+  timeCreated: { type: Date, default: Date.now },
+});
+
+const ReplyModel = mongoose.model<IReply>('Reply', ReplySchema);
+
+export type { IReply };
+export default ReplyModel;
