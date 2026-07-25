05cdbaaa65489a92b11fba7b64162c277ca6dd0e
Jason Paciorek <paciorekjason@gmail.com>
2026-07-25 00:00:52 -0500

    fix: use case-correct user model path

---
+++ fitness-bytes-server/src/models/User.ts
@@ -0,0 +1,22 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IUser extends Document {
+  username: string;
+  password: string;
+  bio: string;
+  profilePicture: Buffer;
+  profilePictureType: string;
+}
+
+const UserSchema: Schema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  bio: { type: String, default: "" },
+  profilePicture: { type: String, default: null },
+  profilePictureType: { type: String, default: ""},
+});
+
+const UserModel = mongoose.model<IUser>('User', UserSchema);
+
+export type { IUser };
+export default UserModel;
