9a99e97e74bfba10c38634f5b657eca78ff44bc2
Jason Paciorek <paciorekjason@gmail.com>
2026-07-25 00:01:31 -0500

    fix: use bcryptjs for password validation

--- fitness-bytes-server/src/libs/Auth.ts
+++ fitness-bytes-server/src/libs/Auth.ts
@@ -1,16 +1,12 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import UserModel from '../models/User';
 
-const SECREYKEY = process.env.SECRETKEY!;
-
 export const validatePassword = async (username: string, password: string): Promise<boolean> => {
-    
-    // Retrieve the hashed password from the database
-    const user = await UserModel.findOne({ username: username }).select('password');
+    // Retrieve the hashed password from the database.
+    const user = await UserModel.findOne({ username }).select('password');
     const retrievedPassword = user?.password;
     if (!retrievedPassword) return false;
 
-    // Compare the entered password with the hashed password
-    const passwordMatch = await bcrypt.compare(password, retrievedPassword);
-    return passwordMatch;
-}
+    // Compare the entered password with the stored hash.
+    return bcrypt.compare(password, retrievedPassword);
+};
